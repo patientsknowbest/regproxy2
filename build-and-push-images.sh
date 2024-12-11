30e4d7d2f3b066027ccb751e7362f71aac80c931
Martin Ashby <martin@ashbysoft.com>
2024-12-11 12:07:48 +0000

    prepare for release 6

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -3,6 +3,6 @@
 ## Pushes to PKB private repository, change the registry if you want to push it elsewhere
 
 ## Increment this number when you push a new image
-VERSION=5
+VERSION=6
 docker build . -t "europe-docker.pkg.dev/infra-240614/eu.gcr.io/regproxy2:$VERSION"
 docker push "europe-docker.pkg.dev/infra-240614/eu.gcr.io/regproxy2:$VERSION"
