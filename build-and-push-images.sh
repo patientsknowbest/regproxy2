920896086aeb16995ccc54dff39b72d888c59804
Martin Ashby <martin@ashbysoft.com>
2026-02-09 14:53:05 +0000

    update version to 8

--- build-and-push-images.sh
+++ build-and-push-images.sh
@@ -3,6 +3,6 @@
 ## Pushes to PKB private repository, change the registry if you want to push it elsewhere
 
 ## Increment this number when you push a new image
-VERSION=8-MFATEST2
+VERSION=8
 docker build . -t "europe-docker.pkg.dev/infra-240614/eu.gcr.io/regproxy2:$VERSION"
 docker push "europe-docker.pkg.dev/infra-240614/eu.gcr.io/regproxy2:$VERSION"
